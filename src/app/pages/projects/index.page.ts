6aeafeeb7a76f82419fd237637626b8f190e89c1
Deepak Paul <rudra.ad7@gmail.com>
2023-07-26 00:12:38 +0530

    feat: projects html added

--- src/app/pages/projects/index.page.ts
+++ src/app/pages/projects/index.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { DataService } from '../shared/data-access/data.service';
+import { DataService } from '../../shared/data-access/data.service';
 import { Observable, tap } from "rxjs";
-import { Project } from "../shared/utils/types";
+import { Project } from "../../shared/utils/types";
 import { CommonModule } from "@angular/common";
 
 
@@ -12,7 +12,7 @@ import { CommonModule } from "@angular/common";
   selector: 'app-projects',
   imports:[CommonModule],
   standalone: true,
-  templateUrl: './projects.view.html',
+  templateUrl: './index.component.html',
 })
 export default class ProjectsComponent {
 
