fff186e4c4e7503858c2d0f72a842fbee5acfcad
Deepak Paul <rudra.ad7@gmail.com>
2023-08-29 23:19:34 +0530

    fix: homepage layout issues

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -12,7 +12,7 @@ import { HeroComponent } from "../components/hero/hero.component";
     
     <app-hero/>
 
-    <section class="h-screen bg-primary relative z-0">
+    <section class="min-h-screen bg-primary relative z-0">
       <div class="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto ">
         <div>
           <h2
@@ -25,6 +25,24 @@ import { HeroComponent } from "../components/hero/hero.component";
         <div class="mt-20 ml-5">
           <app-timeline [events]="work" />
         </div>
+      </div>
+    </section>
+
+
+    <section class="h-screen bg-primary relative z-0">
+      <div class="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto ">
+        <div>
+          <h2
+            class="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]"
+          >
+            Open Source.
+          </h2>
+        </div>
+
+        <div class="mt-20 ml-5">
+        <img src="https://github-readme-stats.vercel.app/api?username=deepakrudrapaul&show_icons=true&theme=transparent&hide=contribs" alt="Github stats">
+        <!-- <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=deepakrudrapaul&theme=transparent" alt="Top languages"/>   -->
+      </div>
       </div>
     </section>
   `,
